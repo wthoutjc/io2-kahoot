bfad9bb5004248814976002ba851f88ba1130907
wthoutjc <juancamilorr2012@hotmail.com>
2022-03-12 21:32:54 -0500

    fix bug

--- src/hooks/urlStates.js
+++ src/hooks/urlStates.js
@@ -1,3 +1,3 @@
-const urlStates = 'http://127.0.0.1:5000'
-// 'https://gltt5focu7.execute-api.sa-east-1.amazonaws.com/prod'
+const urlStates = 'https://gltt5focu7.execute-api.sa-east-1.amazonaws.com/prod'
+// 'http://127.0.0.1:5000'
 export default urlStates
